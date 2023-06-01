b2bf93a8a39a0ffce16438fc40f7ee0c34474869
Danilo Macedo <danilomsp6@gmail.com>
2023-06-01 17:41:14 +0100

    feat: criando o menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,51 @@
+import { Drawer, Box, useTheme, Avatar, Divider, List, ListItemButton, ListItemIcon, ListItemText, Icon } from '@mui/material';
+
+interface IMenuLateralProps {
+    children?: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({children}) => {
+
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant='permanent'>
+        <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
+          
+          <Box 
+            width='100%' 
+            height={theme.spacing(20)} 
+            display='flex' 
+            alignItems='center' 
+            justifyContent='center'
+          >
+            <Avatar 
+              sx={{ width: theme.spacing(12), height: theme.spacing(12)}}
+              src='https://media.licdn.com/dms/image/D4D03AQEMUfS4-4T2FA/profile-displayphoto-shrink_200_200/0/1663778426717?e=1691020800&v=beta&t=w7FDlCXB838_QhRCpmfqCItvYSrkeLQGvsHzyDMfDiQ'/>
+          </Box>
+
+          <Divider />
+          
+          <Box flex={1}>
+            <List component='nav'>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+
+                <ListItemText primary='Página Inicial'/>
+              </ListItemButton>
+
+            </List>
+          </Box>
+
+        </Box>
+      </Drawer>
+      
+      <Box height='100vh' marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
